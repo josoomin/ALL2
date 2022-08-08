16190c6eb54155a364b08929ffaeee80747ae870
josoomin <99530921+josoomin@users.noreply.github.com>
2022-08-08 15:41:42 +0900

    22.08.08

---
+++ Assets/escape room/Scripts/SceneItem/SceneItem.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace roomescape
+{
+    public class SceneItem : MonoBehaviour
+    {
+        public RectTransform _itemTextTrans;
+        public string _itemText = "";
+
+        void Start()
+        {
+            GameObject canvas = UiManager.I.gameObject;
+
+            _itemTextTrans = canvas.transform.Find("ItemText").GetComponent<RectTransform>();
+            _itemTextTrans.gameObject.SetActive(false);
+        }
+
+        void Update()
+        {
+            
+        }
+
+        private void OnMouseEnter()
+        {
+            _itemTextTrans.gameObject.SetActive(true);
+            Text text = _itemTextTrans.GetComponent<Text>();
+            text.text = _itemText;
+        }
+
+        private void OnMouseExit()
+        {
+            _itemTextTrans.gameObject.SetActive(false);
+        }
+
+        void OnMouseDown()
+        {
+            
+        }
+    }
+}
